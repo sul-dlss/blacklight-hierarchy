f0ec881ee1e137422817f2355c4ac56e6d451db5
Naomi Dushay <ndushay@stanford.edu>
2014-07-09 19:26:32 -0700

    basic_spec failing due to mismatch between hierarchy config and facet field names (?)

--- spec/features/basic_spec.rb
+++ spec/features/basic_spec.rb
@@ -5,23 +5,39 @@ describe "catalog" do
   before(:each) do
     CatalogController.blacklight_config = Blacklight::Configuration.new
     CatalogController.configure_blacklight do |config|
-      config.index.title_field = 'title_display'
-      config.default_solr_params = {
-        :rows => 10
-      }
-      response = "{'responseHeader'=>{'status'=>0,'QTime'=>4,'params'=>{'wt'=>'ruby','rows'=>'0'}},'response'=>{'numFound'=>30,'start'=>0,'maxScore'=>1.0,'docs' => [{'id' => '1', 'title_display'=>'title'}]},'facet_counts'=>{'facet_queries'=>{},'facet_fields'=>{'tag_facet'=>['a:b:c',30,'a:b:d', 25, 'a:c:d', 5, 'p:r:q', 25, 'x:y', 5, 'n', 1] }, 'facet_dates'=>{},'facet_ranges'=>{}}}"
-
       config.add_facet_field 'tag_facet', :label => 'Tag', :partial => 'blacklight/hierarchy/facet_hierarchy'
       config.facet_display = {
         :hierarchy => {
           'tag' => [nil]
         }
-      }  
+      }
     end
+    
+    @solr_facet_resp = {'responseHeader'=>{'status'=>0, 'QTime'=>4, 'params'=>{'wt'=>'ruby','rows'=>'0'}},
+                        'response'=>{'numFound'=>30, 'start'=>0, 'maxScore'=>1.0, 'docs' => [{'id' => '1', 'title_display'=>'title'}]},
+                                    'facet_counts' => {
+                                      'facet_queries' => {},
+                                      'facet_fields' => {
+                                        'tag_facet' => [
+                                            'a:b:c', 30,
+                                            'a:b:d', 25, 
+                                            'a:c:d', 5, 
+                                            'p:r:q', 25, 
+                                            'x:y', 5, 
+                                            'n', 1 ] }, 
+                                      'facet_dates' => {},
+                                      'facet_ranges' => {}
+                                    }
+                        }
+
+    rsolr_client = double("rsolr_client")
+    expect(rsolr_client).to receive(:send_and_receive).and_return @solr_facet_resp
+    expect(RSolr).to receive(:connect).and_return rsolr_client
   end
   
   it "should display the hierarchy" do
     visit '/'
+#p page.source 
     page.should have_selector('li.h-node', :content => 'a')
     page.should have_selector('li.h-node > ul > li.h-node', :content => 'b')
     page.should have_selector('li.h-node li.h-leaf', :content => 'c (30)')
