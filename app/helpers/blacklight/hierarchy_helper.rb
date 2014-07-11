34d9b9eafb3c0ca45113e8e8cd161ddd55980595
Naomi Dushay <ndushay@stanford.edu>
2014-07-11 11:26:57 -0700

    improve spec for slash delimiter

--- app/helpers/blacklight/hierarchy_helper.rb
+++ app/helpers/blacklight/hierarchy_helper.rb
@@ -1,165 +1,165 @@
 module Blacklight::HierarchyHelper
 
-def is_hierarchical?(field_name)
-  (prefix,order) = field_name.split(/_/, 2)
-  list = blacklight_config.facet_display[:hierarchy][prefix] and list.include?(order)
-end
-    
-def facet_order(prefix)
-  param_name = "#{prefix}_facet_order".to_sym
-  params[param_name] || blacklight_config.facet_display[:hierarchy][prefix].first
-end
-
-def facet_after(prefix, order)
-  orders = blacklight_config.facet_display[:hierarchy][prefix]
-  orders[orders.index(order)+1] || orders.first
-end
-
-def hide_facet?(field_name)
-  if is_hierarchical?(field_name)
-    prefix = field_name.split(/_/).first
-    field_name != "#{prefix}_#{facet_order(prefix)}"
-  else
-    false
-  end
-end
-
-def rotate_facet_value(val, from, to)
-  components = Hash[from.split(//).zip(val.split(/:/))]
-  new_values = components.values_at(*(to.split(//)))
-  while new_values.last.nil?
-    new_values.pop
-  end
-  if new_values.include?(nil)
-    nil
-  else
-    new_values.compact.join(':')
-  end
-end
-
-def rotate_facet_params(prefix, from, to, p=params.dup)
-  return p if from == to
-  from_field = "#{prefix}_#{from}"
-  to_field = "#{prefix}_#{to}"
-  p[:f] = (p[:f] || {}).dup # the command above is not deep in rails3, !@#$!@#$
-  p[:f][from_field] = (p[:f][from_field] || []).dup
-  p[:f][to_field] = (p[:f][to_field] || []).dup
-  p[:f][from_field].reject! { |v| p[:f][to_field] << rotate_facet_value(v, from, to); true }
-  p[:f].delete(from_field)
-  p[:f][to_field].compact!
-  p[:f].delete(to_field) if p[:f][to_field].empty?
-  p
-end
-
-def render_facet_rotate(field_name)
-  if is_hierarchical?(field_name)
+  def is_hierarchical?(field_name)
     (prefix,order) = field_name.split(/_/, 2)
-
-    return if blacklight_config.facet_display[:hierarchy][prefix].length < 2
-
-    new_order = facet_after(prefix,order)
-    new_params = rotate_facet_params(prefix,order,new_order)
-    new_params["#{prefix}_facet_order"] = new_order
-    link_to image_tag('icons/rotate.png', :title => new_order.upcase).html_safe, new_params, :class => 'no-underline'
+    list = blacklight_config.facet_display[:hierarchy][prefix] and list.include?(order)
   end
-end
-
-# Putting bare HTML strings in a helper sucks. But in this case, with a 
-# lot of recursive tree-walking going on, it's an order of magnitude faster
-# than either render(:partial) or content_tag
-def render_facet_hierarchy_item(field_name, data, key)
-  item = data[:_]
-  subset = data.reject { |k,v| ! k.is_a?(String) }
-  
-  li_class = subset.empty? ? 'h-leaf' : 'h-node'
-  li = ul = ''
-  
-  if item.nil?
-    li = key
-  elsif facet_in_params?(field_name, item.qvalue)
-    li = render_selected_qfacet_value(field_name, item)
-  else
-    li = render_qfacet_value(field_name, item)
-  end
-  
-  unless subset.empty?
-    subul = subset.keys.sort.collect do |subkey| 
-      render_facet_hierarchy_item(field_name, subset[subkey], subkey) 
-    end.join('')
-    ul = "<ul>#{subul}</ul>".html_safe
-  end
-  
-  %{<li class="#{li_class}">#{li.html_safe}#{ul.html_safe}</li>}.html_safe
-end
-
-# TODO:  document the rotate
-# 
-# TODO:  write specs for each non-trivial helper method (?)
-
-# TODO: remove baked in notion of underscores being part of the blacklight facet field names, 
-# and of _facet being the suffix of the Solr field name seems to be baked in.
-# 
-# @param [Blacklight::Configuration::FacetField] as defined in controller with config.add_facet_field (and with :partial => 'blacklight/hierarchy/facet_hierarchy')
-# @return [String] html for the facet tree
-def render_hierarchy(bl_facet_field)
-  field_name = bl_facet_field.field
-  prefix = field_name.split(/_/).first
-  tree = facet_tree(prefix)[field_name]
-  if tree
-    result = tree.keys.sort.collect do |key|
-      render_facet_hierarchy_item(field_name, tree[key], key)
-    end.join("\n").html_safe
-  else
-    ""
-  end
-end
-
-def render_qfacet_value(facet_solr_field, item, options ={})    
-  (link_to_unless(options[:suppress_link], item.value, add_facet_params(facet_solr_field, item.qvalue), :class=>"facet_select label") + " " + render_facet_count(item.hits)).html_safe
-end
-
-# Standard display of a SELECTED facet value, no link, special span
-# with class, and 'remove' button.
-def render_selected_qfacet_value(facet_solr_field, item)
-  content_tag(:span, render_qfacet_value(facet_solr_field, item, :suppress_link => true), :class => "selected label") +
-    link_to("[remove]", remove_facet_params(facet_solr_field, item.qvalue, params), :class=>"remove")
+    
+  def facet_order(prefix)
+    param_name = "#{prefix}_facet_order".to_sym
+    params[param_name] || blacklight_config.facet_display[:hierarchy][prefix].first
   end
 
-HierarchicalFacetItem = Struct.new :qvalue, :value, :hits
-# @param [String] hkey - a key to access the rest of the hierarchy tree, as defined in controller config.facet_display[:hierarchy] declaration.
-#  e.g. if you had this in controller:
-#   config.facet_display = {
-#     :hierarchy => {
-#       'wf' => [['wps','wsp','swp'], ':'],
-#       'callnum' => [['top_facet'], '/']
-#    }
-#  }
-# then possible hkey values would be 'wf' and 'callnum'
-def facet_tree(hkey)
-  @facet_tree ||= {}
-  if blacklight_config.facet_display[:hierarchy] && blacklight_config.facet_display[:hierarchy][hkey] 
-    value_delim = blacklight_config.facet_display[:hierarchy][hkey].last
-    split_regex = Regexp.new("\s*#{value_delim}\s*")
-    if @facet_tree[hkey].nil?
-      @facet_tree[hkey] = {}
-      blacklight_config.facet_display[:hierarchy][hkey].first.each { |key|
-        facet_field = [hkey,key].compact.join('_')
-        @facet_tree[hkey][facet_field] ||= {}
-        data = @response.facet_by_field_name(facet_field)
-        next if data.nil?
+  def facet_after(prefix, order)
+    orders = blacklight_config.facet_display[:hierarchy][prefix]
+    orders[orders.index(order)+1] || orders.first
+  end
 
-        data.items.each { |facet_item|
-          path = facet_item.value.split(split_regex)
-          loc = @facet_tree[hkey][facet_field]
-          while path.length > 0
-            loc = loc[path.shift] ||= {}
-          end
-          loc[:_] = HierarchicalFacetItem.new(facet_item.value, facet_item.value.split(split_regex).last, facet_item.hits)
-        }
-      }
+  def hide_facet?(field_name)
+    if is_hierarchical?(field_name)
+      prefix = field_name.split(/_/).first
+      field_name != "#{prefix}_#{facet_order(prefix)}"
+    else
+      false
     end
   end
-  @facet_tree[hkey]
-end
+
+  def rotate_facet_value(val, from, to)
+    components = Hash[from.split(//).zip(val.split(/:/))]
+    new_values = components.values_at(*(to.split(//)))
+    while new_values.last.nil?
+      new_values.pop
+    end
+    if new_values.include?(nil)
+      nil
+    else
+      new_values.compact.join(':')
+    end
+  end
+
+  def rotate_facet_params(prefix, from, to, p=params.dup)
+    return p if from == to
+    from_field = "#{prefix}_#{from}"
+    to_field = "#{prefix}_#{to}"
+    p[:f] = (p[:f] || {}).dup # the command above is not deep in rails3, !@#$!@#$
+    p[:f][from_field] = (p[:f][from_field] || []).dup
+    p[:f][to_field] = (p[:f][to_field] || []).dup
+    p[:f][from_field].reject! { |v| p[:f][to_field] << rotate_facet_value(v, from, to); true }
+    p[:f].delete(from_field)
+    p[:f][to_field].compact!
+    p[:f].delete(to_field) if p[:f][to_field].empty?
+    p
+  end
+
+  def render_facet_rotate(field_name)
+    if is_hierarchical?(field_name)
+      (prefix,order) = field_name.split(/_/, 2)
+
+      return if blacklight_config.facet_display[:hierarchy][prefix].length < 2
+
+      new_order = facet_after(prefix,order)
+      new_params = rotate_facet_params(prefix,order,new_order)
+      new_params["#{prefix}_facet_order"] = new_order
+      link_to image_tag('icons/rotate.png', :title => new_order.upcase).html_safe, new_params, :class => 'no-underline'
+    end
+  end
+
+  # Putting bare HTML strings in a helper sucks. But in this case, with a 
+  # lot of recursive tree-walking going on, it's an order of magnitude faster
+  # than either render(:partial) or content_tag
+  def render_facet_hierarchy_item(field_name, data, key)
+    item = data[:_]
+    subset = data.reject { |k,v| ! k.is_a?(String) }
+  
+    li_class = subset.empty? ? 'h-leaf' : 'h-node'
+    li = ul = ''
+  
+    if item.nil?
+      li = key
+    elsif facet_in_params?(field_name, item.qvalue)
+      li = render_selected_qfacet_value(field_name, item)
+    else
+      li = render_qfacet_value(field_name, item)
+    end
+  
+    unless subset.empty?
+      subul = subset.keys.sort.collect do |subkey| 
+        render_facet_hierarchy_item(field_name, subset[subkey], subkey) 
+      end.join('')
+      ul = "<ul>#{subul}</ul>".html_safe
+    end
+  
+    %{<li class="#{li_class}">#{li.html_safe}#{ul.html_safe}</li>}.html_safe
+  end
+
+  # TODO:  document the rotate
+  # 
+  # TODO:  write specs for each non-trivial helper method (?)
+
+  # TODO: remove baked in notion of underscores being part of the blacklight facet field names, 
+  # and of _facet being the suffix of the Solr field name seems to be baked in.
+  # 
+  # @param [Blacklight::Configuration::FacetField] as defined in controller with config.add_facet_field (and with :partial => 'blacklight/hierarchy/facet_hierarchy')
+  # @return [String] html for the facet tree
+  def render_hierarchy(bl_facet_field)
+    field_name = bl_facet_field.field
+    prefix = field_name.split(/_/).first
+    tree = facet_tree(prefix)[field_name]
+    if tree
+      result = tree.keys.sort.collect do |key|
+        render_facet_hierarchy_item(field_name, tree[key], key)
+      end.join("\n").html_safe
+    else
+      ""
+    end
+  end
+
+  def render_qfacet_value(facet_solr_field, item, options ={})    
+    (link_to_unless(options[:suppress_link], item.value, add_facet_params(facet_solr_field, item.qvalue), :class=>"facet_select label") + " " + render_facet_count(item.hits)).html_safe
+  end
+
+  # Standard display of a SELECTED facet value, no link, special span
+  # with class, and 'remove' button.
+  def render_selected_qfacet_value(facet_solr_field, item)
+    content_tag(:span, render_qfacet_value(facet_solr_field, item, :suppress_link => true), :class => "selected label") +
+      link_to("[remove]", remove_facet_params(facet_solr_field, item.qvalue, params), :class=>"remove")
+    end
+
+  HierarchicalFacetItem = Struct.new :qvalue, :value, :hits
+  # @param [String] hkey - a key to access the rest of the hierarchy tree, as defined in controller config.facet_display[:hierarchy] declaration.
+  #  e.g. if you had this in controller:
+  #   config.facet_display = {
+  #     :hierarchy => {
+  #       'wf' => [['wps','wsp','swp'], ':'],
+  #       'callnum' => [['top_facet'], '/']
+  #    }
+  #  }
+  # then possible hkey values would be 'wf' and 'callnum'
+  def facet_tree(hkey)
+    @facet_tree ||= {}
+    if blacklight_config.facet_display[:hierarchy] && blacklight_config.facet_display[:hierarchy][hkey] 
+      value_delim = blacklight_config.facet_display[:hierarchy][hkey].last
+      split_regex = Regexp.new("\s*#{value_delim}\s*")
+      if @facet_tree[hkey].nil?
+        @facet_tree[hkey] = {}
+        blacklight_config.facet_display[:hierarchy][hkey].first.each { |key|
+          facet_field = [hkey,key].compact.join('_')
+          @facet_tree[hkey][facet_field] ||= {}
+          data = @response.facet_by_field_name(facet_field)
+          next if data.nil?
+
+          data.items.each { |facet_item|
+            path = facet_item.value.split(split_regex)
+            loc = @facet_tree[hkey][facet_field]
+            while path.length > 0
+              loc = loc[path.shift] ||= {}
+            end
+            loc[:_] = HierarchicalFacetItem.new(facet_item.value, facet_item.value.split(split_regex).last, facet_item.hits)
+          }
+        }
+      end
+    end
+    @facet_tree[hkey]
+  end
 
 end
--- spec/features/basic_spec.rb
+++ spec/features/basic_spec.rb
@@ -32,9 +32,9 @@ describe "catalog" do
                                             'x:y', 5, 
                                             'n', 1 ],
                                         'my_top_facet' => [
-                                            'a/b/c', 30,
-                                            'x/y', 5, 
-                                            'n', 1 ], 
+                                            'f/g/h', 30,
+                                            'j/k', 5, 
+                                            'z', 1 ], 
                                        }, 
                                       'facet_dates' => {},
                                       'facet_ranges' => {}
@@ -69,43 +69,14 @@ describe "catalog" do
     expect(page.all(:css, 'li.h-leaf a').map { |a| a[:href].to_s }).to include(catalog_index_path('f' => { 'tag_facet' => ['x:y'] }))
   end
 
-  it "should work with a different delimiter" do
-=begin
-    CatalogController.blacklight_config = Blacklight::Configuration.new
-    CatalogController.configure_blacklight do |config|
-      config.add_facet_field 'my_top_facet', :label => 'Slash Delim', :partial => 'blacklight/hierarchy/facet_hierarchy'
-      config.facet_display = {
-        :hierarchy => {
-          'my' => [['top_facet'], '/']
-        }
-      }
-    end
-=end
-=begin
-    solr_resp = {'responseHeader'=>{'status'=>0, 'QTime'=>4, 'params'=>{'wt'=>'ruby','rows'=>'0'}},
-                  'response'=>{'numFound'=>30, 'start'=>0, 'maxScore'=>1.0, 'docs' => []},
-                              'facet_counts' => {
-                                'facet_queries' => {},
-                                'facet_fields' => {
-                                  'my_top_facet' => [
-                                      'a/b/c', 30,
-                                      'x/y', 5, 
-                                      'n', 1 ] }, 
-                                'facet_dates' => {},
-                                'facet_ranges' => {}
-                              }
-                }
-    rsolr_client = double("rsolr_client")
-    expect(rsolr_client).to receive(:send_and_receive).and_return solr_resp
-    expect(RSolr).to receive(:connect).and_return rsolr_client
-=end
+  it "should work with a different value delimiter" do
     visit '/'
-    expect(page).to have_selector('li.h-node', :text => 'a')
-    expect(page).to have_selector('li.h-node > ul > li.h-node', :text => 'b')
-    expect(page).to have_selector('li.h-node li.h-leaf', :text => 'c 30')
-    expect(page).to have_selector('li.h-node', :text => 'x')
-    expect(page).to have_selector('li.h-node li.h-leaf', :text => 'y 5')
-    expect(page).to have_selector('.facet-hierarchy > li.h-leaf', :text => 'n 1')
+    expect(page).to have_selector('li.h-node', :text => 'f')
+    expect(page).to have_selector('li.h-node > ul > li.h-node', :text => 'g')
+    expect(page).to have_selector('li.h-node li.h-leaf', :text => 'h 30')
+    expect(page).to have_selector('li.h-node', :text => 'j')
+    expect(page).to have_selector('li.h-node li.h-leaf', :text => 'k 5')
+    expect(page).to have_selector('.facet-hierarchy > li.h-leaf', :text => 'z 1')
   end
 
 end
