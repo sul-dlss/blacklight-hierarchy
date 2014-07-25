4756a435f97f82de31850306f40c61d156690965
Naomi Dushay <ndushay@stanford.edu>
2014-07-24 18:15:09 -0700

    move rotate helper methods down out of the way

--- app/helpers/blacklight/hierarchy_helper.rb
+++ app/helpers/blacklight/hierarchy_helper.rb
@@ -1,74 +1,5 @@
 module Blacklight::HierarchyHelper
 
-  # FIXME:  remove baked in underscore separator in field name
-  def is_hierarchical?(field_name)
-    (prefix,order) = field_name.split(/_/, 2)
-    list = blacklight_config.facet_display[:hierarchy][prefix] and list.include?(order)
-  end
-    
-  def facet_order(prefix)
-    param_name = "#{prefix}_facet_order".to_sym
-    params[param_name] || blacklight_config.facet_display[:hierarchy][prefix].first
-  end
-
-  def facet_after(prefix, order)
-    orders = blacklight_config.facet_display[:hierarchy][prefix]
-    orders[orders.index(order)+1] || orders.first
-  end
-
-  # FIXME:  remove baked in underscore separator in field name
-  def hide_facet?(field_name)
-    if is_hierarchical?(field_name)
-      prefix = field_name.split(/_/).first
-      field_name != "#{prefix}_#{facet_order(prefix)}"
-    else
-      false
-    end
-  end
-
-  # FIXME:  remove baked in colon separator
-  def rotate_facet_value(val, from, to)
-    components = Hash[from.split(//).zip(val.split(/:/))]
-    new_values = components.values_at(*(to.split(//)))
-    while new_values.last.nil?
-      new_values.pop
-    end
-    if new_values.include?(nil)
-      nil
-    else
-      new_values.compact.join(':')
-    end
-  end
-
-  # FIXME:  remove baked in underscore separator in field name
-  def rotate_facet_params(prefix, from, to, p=params.dup)
-    return p if from == to
-    from_field = "#{prefix}_#{from}"
-    to_field = "#{prefix}_#{to}"
-    p[:f] = (p[:f] || {}).dup # the command above is not deep in rails3, !@#$!@#$
-    p[:f][from_field] = (p[:f][from_field] || []).dup
-    p[:f][to_field] = (p[:f][to_field] || []).dup
-    p[:f][from_field].reject! { |v| p[:f][to_field] << rotate_facet_value(v, from, to); true }
-    p[:f].delete(from_field)
-    p[:f][to_field].compact!
-    p[:f].delete(to_field) if p[:f][to_field].empty?
-    p
-  end
-
-  # FIXME:  remove baked in underscore separator in field name
-  def render_facet_rotate(field_name)
-    if is_hierarchical?(field_name)
-      (prefix,order) = field_name.split(/_/, 2)
-
-      return if blacklight_config.facet_display[:hierarchy][prefix].length < 2
-
-      new_order = facet_after(prefix,order)
-      new_params = rotate_facet_params(prefix,order,new_order)
-      new_params["#{prefix}_facet_order"] = new_order
-      link_to image_tag('icons/rotate.png', :title => new_order.upcase).html_safe, new_params, :class => 'no-underline'
-    end
-  end
-
   # Putting bare HTML strings in a helper sucks. But in this case, with a 
   # lot of recursive tree-walking going on, it's an order of magnitude faster
   # than either render(:partial) or content_tag
@@ -162,6 +93,77 @@ module Blacklight::HierarchyHelper
       end
     end
     @facet_tree[hkey]
+  end
+
+  # below are methods pertaining to the "rotate" notion where you may want to look at the same tree data organized another way
+  
+  # FIXME:  remove baked in underscore separator in field name
+  def is_hierarchical?(field_name)
+    (prefix,order) = field_name.split(/_/, 2)
+    list = blacklight_config.facet_display[:hierarchy][prefix] and list.include?(order)
+  end
+    
+  def facet_order(prefix)
+    param_name = "#{prefix}_facet_order".to_sym
+    params[param_name] || blacklight_config.facet_display[:hierarchy][prefix].first
+  end
+
+  def facet_after(prefix, order)
+    orders = blacklight_config.facet_display[:hierarchy][prefix]
+    orders[orders.index(order)+1] || orders.first
+  end
+
+  # FIXME:  remove baked in underscore separator in field name
+  def hide_facet?(field_name)
+    if is_hierarchical?(field_name)
+      prefix = field_name.split(/_/).first
+      field_name != "#{prefix}_#{facet_order(prefix)}"
+    else
+      false
+    end
+  end
+  
+  # FIXME:  remove baked in colon separator
+  def rotate_facet_value(val, from, to)
+    components = Hash[from.split(//).zip(val.split(/:/))]
+    new_values = components.values_at(*(to.split(//)))
+    while new_values.last.nil?
+      new_values.pop
+    end
+    if new_values.include?(nil)
+      nil
+    else
+      new_values.compact.join(':')
+    end
+  end
+
+  # FIXME:  remove baked in underscore separator in field name
+  def rotate_facet_params(prefix, from, to, p=params.dup)
+    return p if from == to
+    from_field = "#{prefix}_#{from}"
+    to_field = "#{prefix}_#{to}"
+    p[:f] = (p[:f] || {}).dup # the command above is not deep in rails3, !@#$!@#$
+    p[:f][from_field] = (p[:f][from_field] || []).dup
+    p[:f][to_field] = (p[:f][to_field] || []).dup
+    p[:f][from_field].reject! { |v| p[:f][to_field] << rotate_facet_value(v, from, to); true }
+    p[:f].delete(from_field)
+    p[:f][to_field].compact!
+    p[:f].delete(to_field) if p[:f][to_field].empty?
+    p
+  end
+
+  # FIXME:  remove baked in underscore separator in field name
+  def render_facet_rotate(field_name)
+    if is_hierarchical?(field_name)
+      (prefix,order) = field_name.split(/_/, 2)
+
+      return if blacklight_config.facet_display[:hierarchy][prefix].length < 2
+
+      new_order = facet_after(prefix,order)
+      new_params = rotate_facet_params(prefix,order,new_order)
+      new_params["#{prefix}_facet_order"] = new_order
+      link_to image_tag('icons/rotate.png', :title => new_order.upcase).html_safe, new_params, :class => 'no-underline'
+    end
   end
 
 end
