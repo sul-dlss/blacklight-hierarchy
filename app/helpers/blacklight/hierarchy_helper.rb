f5b197f04d053199f4b4a0e5f24e7559e130c4e0
Justin Coyne <jcoyne@justincoyne.com>
2021-04-15 23:49:24 -0500

    Add aria-controls for better accessibility

--- app/helpers/blacklight/hierarchy_helper.rb
+++ app/helpers/blacklight/hierarchy_helper.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
 module Blacklight::HierarchyHelper
-  def facet_toggle_button(field_name, described_by)
+  def facet_toggle_button(field_name, described_by, controls)
     aria_label = I18n.t(
       "blacklight.hierarchy.#{field_name}_toggle_aria_label",
       default: :'blacklight.hierarchy.toggle_aria_label'
     )
 
     # For Rails 5.2 support all options must be symbols.  See https://github.com/rails/rails/issues/39813
-    tag.button(:'aria-expanded' => 'false',
-                :'aria-label' => aria_label,
-                :'aria-describedby' => described_by,
-                data: { action: 'click->b-h-collapsible#toggle' },
-                class: 'toggle-handle') do
+    tag.button(aria: {
+                 expanded: 'false',
+                 label: aria_label,
+                 describedby: described_by,
+                 controls: controls
+               },
+               data: { action: 'click->b-h-collapsible#toggle' },
+               class: 'toggle-handle') do
       tag.span(Blacklight::Hierarchy::Engine.config.closed_icon, :'aria-hidden' => 'true', class: 'closed') +
       tag.span(Blacklight::Hierarchy::Engine.config.opened_icon, :'aria-hidden' => 'true', class: 'opened')
     end
--- spec/helpers/hierarchy_helper_spec.rb
+++ spec/helpers/hierarchy_helper_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Blacklight::HierarchyHelper do
   describe '#facet_toggle_button' do
-    subject { helper.facet_toggle_button(field_name, described_by) }
+    subject { helper.facet_toggle_button(field_name, described_by, 'randomtext123') }
     let(:field_name) { 'exploded_tag_ssim' }
     let(:described_by) { 'unique-string' }
 
