41b4120309cc059a2d42cd37e6069ad1c446ed84
Justin Coyne <jcoyne@justincoyne.com>
2021-12-06 13:43:10 -0600

    Add bootstrap 5 data attributes for collapsable behavior

--- app/helpers/blacklight/hierarchy_helper.rb
+++ app/helpers/blacklight/hierarchy_helper.rb
@@ -17,7 +17,9 @@ module Blacklight::HierarchyHelper
                data: {
                  action: 'click->b-h-collapsible#toggle',
                  toggle: 'collapse',
-                 target: "##{controls}"
+                 bs_toggle: 'collapse',
+                 target: "##{controls}",
+                 bs_target: "##{controls}"
                },
                class: 'toggle-handle') do
       tag.span(Blacklight::Hierarchy::Engine.config.closed_icon, :'aria-hidden' => 'true', class: 'closed') +
