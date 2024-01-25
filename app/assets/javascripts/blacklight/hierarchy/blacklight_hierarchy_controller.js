49b21a9a48accef56f10bea8e681ec0da1453f38
Justin Coyne <jcoyne@justincoyne.com>
2024-01-25 15:28:43 -0600

    Use the bootstrap 4 class for collapse component

--- app/assets/javascripts/blacklight/hierarchy/blacklight_hierarchy_controller.js
+++ app/assets/javascripts/blacklight/hierarchy/blacklight_hierarchy_controller.js
@@ -4,6 +4,13 @@ export default class extends Controller {
   static targets = [ "list" ]
   connect() {
     this.element.classList.add("twiddle")
+
+    // If a child facet-value is selected, then expand the node
+    if (this.element.querySelector(':scope > span.selected')) {
+      this.element.classList.add('twiddle-open')
+      this.element.querySelectorAll(':scope > .collapse')
+        .forEach((collapsable) => collapsable.classList.add('show'))
+    }
   }
 
   toggle() {
