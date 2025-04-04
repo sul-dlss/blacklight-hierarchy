e9ddeb3355af4ba23959d7f8ff02f7c1420eb37b
Justin Coyne <jcoyne@justincoyne.com>
2025-04-04 10:07:54 -0500

    Remove the jquery dependency

--- app/assets/javascripts/blacklight/hierarchy/hierarchy.js
+++ app/assets/javascripts/blacklight/hierarchy/hierarchy.js
@@ -1,31 +1,31 @@
-Blacklight.onLoad(function(){
-  Blacklight.do_hierarchical_facet_expand_contract_behavior();
-});
+Blacklight.onLoad(() => Blacklight.do_hierarchical_facet_expand_contract_behavior())
 
-(function($) {
-  Blacklight.do_hierarchical_facet_expand_contract_behavior = function() {
-    $( Blacklight.do_hierarchical_facet_expand_contract_behavior.selector ).each (
-        Blacklight.hierarchical_facet_expand_contract
-     );
+(() => {
+  Blacklight.do_hierarchical_facet_expand_contract_behavior = () => {
+    const elements = document.querySelectorAll(Blacklight.do_hierarchical_facet_expand_contract_behavior.selector)
+    elements.forEach(elem => Blacklight.hierarchical_facet_expand_contract(elem))
   }
-  Blacklight.do_hierarchical_facet_expand_contract_behavior.selector = '[data-controller="b-h-collapsible"]';
-  Blacklight.do_hierarchical_facet_expand_contract_behavior.handle = '[data-action="click->b-h-collapsible#toggle"]';
-  Blacklight.do_hierarchical_facet_expand_contract_behavior.list = '[data-b-h-collapsible-target="list"]';
+  Blacklight.do_hierarchical_facet_expand_contract_behavior.selector = '[data-controller="b-h-collapsible"]'
+  Blacklight.do_hierarchical_facet_expand_contract_behavior.handle = '[data-action="click->b-h-collapsible#toggle"]'
+  Blacklight.do_hierarchical_facet_expand_contract_behavior.list = '[data-b-h-collapsible-target="list"]'
 
-  Blacklight.hierarchical_facet_expand_contract = function() {
-    var li = $(this);
-    li.addClass('twiddle');
+  Blacklight.hierarchical_facet_expand_contract = (element) => {
+    element.classList.add('twiddle')
 
-    $(Blacklight.do_hierarchical_facet_expand_contract_behavior.list, this).each(function() {
-      if($('span.selected', this).length != 0){
-        li.addClass('twiddle-open');
-        li.children('.collapse').addClass('show');
+    const lists = element.querySelectorAll(Blacklight.do_hierarchical_facet_expand_contract_behavior.list)
+
+    lists.forEach((list) => {
+      if (list.querySelector('span.selected')) {
+        element.classList.add('twiddle-open')
+        const collapseElement = element.querySelector('.collapse')
+        if (collapseElement) {
+          collapseElement.classList.add('show')
+        }
       }
-    });
+    })
 
     // attach the toggle behavior to the li tag
-    li.children(Blacklight.do_hierarchical_facet_expand_contract_behavior.handle).click(function(e){
-      li.toggleClass('twiddle-open');
-    });
-  };
-})(jQuery);
+    const handle = element.querySelector(Blacklight.do_hierarchical_facet_expand_contract_behavior.handle)
+    handle?.addEventListener('click', () => element.classList.toggle('twiddle-open'))
+  }
+})()
