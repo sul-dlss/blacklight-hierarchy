653a1251654e96d3aaf5485bcb3134a30eb7be0e
Justin Coyne <jcoyne@justincoyne.com>
2025-07-08 15:20:04 -0500

    Update remove facet button to use Bootstrap 5 styles

--- app/components/blacklight/hierarchy/selected_qfacet_value_component.rb
+++ app/components/blacklight/hierarchy/selected_qfacet_value_component.rb
@@ -3,6 +3,7 @@
 module Blacklight
   module Hierarchy
     # Standard display of a SELECTED facet value, no link, special span with class, and 'remove' button.
+    # This is similar to Blacklight::Facet::SelectedValueComponent
     class SelectedQfacetValueComponent < QfacetValueComponent
       def initialize(field_name:, item:)
         @field_name = field_name
@@ -13,6 +14,13 @@ module Blacklight
 
       def remove_href
         helpers.search_action_path(helpers.search_state.filter(field_name).remove(item.qvalue).params)
+      end
+
+      def remove_link
+        link_to(remove_href, class: "remove ps-2", rel: "nofollow") do
+          render(Blacklight::Icons::RemoveComponent.new(aria_hidden: true)) +
+            tag.span(t(:'blacklight.search.facets.selected.remove'), class: 'visually-hidden')
+        end
       end
     end
   end
